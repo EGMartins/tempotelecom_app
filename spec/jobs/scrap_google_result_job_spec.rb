3833fc3dc5dd07c52fc8e1e85ee8576efe30ed67
Eduardo Gomes Martins <mirassol.code@gmail.com>
2023-04-07 20:06:49 -0300

    Fix the test runs issue

--- spec/jobs/scrap_google_result_job_spec.rb
+++ spec/jobs/scrap_google_result_job_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe ScrapGoogleResultJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  include ActiveJob::TestHelper
+
+  subject(:job) { described_class.perform_later }
+
+  it 'queues the job' do
+    expect { job }
+      .to change(ActiveJob::Base.queue_adapter.enqueued_jobs, :size).by(1)
+  end
+
+  it 'is in medium queue' do
+    expect(ScrapGoogleResultJob.new.queue_name).to eq('default')
+  end
+
+  it { is_expected.to be_processed_in :default }
+
+  after do
+    clear_enqueued_jobs
+    clear_performed_jobs
+  end
 end
